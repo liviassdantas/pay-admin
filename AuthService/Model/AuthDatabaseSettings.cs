7cf89746463bd24ca0591f5f137c270431863c2a
Livia Dantas <livia.ss.dantas@gmail.com>
2023-11-08 15:20:18 -0300

    Adding database mongo configuration

---
+++ AuthService/Model/AuthDatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthService.Model
+{
+    public class AuthDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+        public string DatabaseName { get; set; } = null!;
+        public string CollectionName { get; set; } = null!;
+    }
+}
