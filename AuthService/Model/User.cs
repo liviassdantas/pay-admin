6922bdcaafb9146e38ac04048f6fcf0f28bb7c3c
Livia Dantas <livia.ss.dantas@gmail.com>
2023-11-08 15:43:38 -0300

    creating user class

---
+++ AuthService/Model/User.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Diagnostics.CodeAnalysis;
+
+namespace AuthService.Model
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Email")]
+        public string Email { get; set; }
+
+        [BsonElement("Password")]
+        public string Password { get; set; }
+
+        [BsonElement("IsAdmin")]
+        public bool IsAdmin { get; set; }
+    }
+}
