f2cac857135079b67cfe826ea61be2c54d5408cb
Livia Dantas <livia.ss.dantas@gmail.com>
2023-11-11 20:56:37 -0300

    model paymentTransaction created

---
+++ PaymentService/Model/PaymentTransaction.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using pay_admin.Model.Enum;
+using System.Reflection.Metadata;
+
+namespace pay_admin.Model
+{
+    public class PaymentTransaction
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ObjectId { get; set; }
+
+        [BsonElement("IdUser")]
+        public string UserID { get; set; }
+
+        [BsonElement("TransactionID")]
+        public string TransactionID { get; set; }
+
+        [BsonElement("PaymentStatus")]
+        public string PaymentStatus { get; set; }
+
+        [BsonElement("QrCode")]
+        public string QrCode { get; set; }
+
+    }
+}
