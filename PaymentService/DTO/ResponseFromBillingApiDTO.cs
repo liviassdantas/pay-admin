d6639e0cfe52fe2676e93e803e615eb4965d5481
Livia Dantas <livia.ss.dantas@gmail.com>
2023-11-12 00:33:20 -0300

    creating billing api ready

---
+++ PaymentService/DTO/ResponseFromBillingApiDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace pay_admin.DTO
+{
+    public class ResponseFromBillingApiDTO
+    {
+        public string message { get; set; }
+        public string ID { get; set; }
+        public DateTime timestamp { get; set; }
+        public string status { get; set; }
+        public string qrCode { get; set; }
+    }
+}
--- PaymentService/Service/PaymentService.cs
+++ PaymentService/Service/PaymentService.cs
@@ -6,9 +6,13 @@ using pay_admin.DTO;
 using pay_admin.Interfaces;
 using pay_admin.Model;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Net;
 using System.Net.Http.Headers;
 using System.Security.Claims;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace pay_admin.Service
 {
@@ -18,7 +22,7 @@ namespace pay_admin.Service
 
         private readonly IMongoCollection<PaymentTransaction> _paymentTransactionCollection;
         private static HttpClient client = new();
-        private static Object loggedUser = new Object();
+        
 
         public PaymentService(IOptions<PaymentsDatabaseSettings> paymentDatabaseSettings)
         {
@@ -27,13 +31,58 @@ namespace pay_admin.Service
             _paymentTransactionCollection = mongoDatabase.GetCollection<PaymentTransaction>(paymentDatabaseSettings.Value.CollectionName);
         }
 
-        public async Task<HttpResponse> CreateNewBilling(HttpContext context, PaymentTransactionDTO billing)
+        public async Task<HttpResponseMessage> CreateNewBilling(HttpContext context, PaymentTransactionDTO billing)
         {
+            try
+            {
+                var returnStatus = new HttpStatusCode();
+                var loggedUser = GetLoggedUser(context);
+                billing.UserID = loggedUser.FirstOrDefault().Key;
+
+                var createBillingApi = await client.PostAsync("https://eoc56jqea5ysq7e.m.pipedream.net", JsonContent.Create(new { value = 10.8 }));
+                
+                if(createBillingApi.IsSuccessStatusCode && createBillingApi.Content != null)
+                {
+                    var resultJson = await createBillingApi.Content.ReadAsStringAsync();
+                    var result = JsonSerializer.Deserialize<ResponseFromBillingApiDTO>(resultJson);
+
+                    if (!String.IsNullOrEmpty(result?.ID))
+                    {
+                        billing.PaymentStatus = result.status;
+                        billing.TransactionID = result.ID;
+                        billing.QrCode = result.qrCode;
+                        billing.PaymentStatus = result.status;
+
+                        var paymentTransaction = new PaymentTransaction
+                        {
+                            TransactionID = billing.TransactionID,
+                            UserID = billing.UserID,
+                            PaymentStatus = billing.PaymentStatus,
+                            QrCode = billing.QrCode,
+                        };
+
+                        await _paymentTransactionCollection.InsertOneAsync(paymentTransaction);
+                        returnStatus = HttpStatusCode.Created;
+                    }
+                    else
+                    {
+                        returnStatus = HttpStatusCode.BadRequest;
+                    }
+                    
+                }
+                return new HttpResponseMessage(returnStatus);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
 
         }
 
-        private static async void GetLoggedUser(HttpContext context)
+        private static Dictionary<string, string> GetLoggedUser(HttpContext context)
         {
+            var loggedUser = new Dictionary<string, string>(); 
             if (context.User.Identity?.Name != null)
             {
                 var userIdentity = new
@@ -41,8 +90,9 @@ namespace pay_admin.Service
                     Email = context.User.Identity.Name,
                     Role = context.User?.Claims?.FirstOrDefault(x => x?.Type == ClaimTypes.Role)?.ValueType ?? ""
                 };
-
-                loggedUser = userIdentity;
+                loggedUser.Add(userIdentity.Email, userIdentity.Role);
+                
+                return loggedUser;
             }
             else
             {
