8beebbf5d4b69c3834f20586e6a4c5660b7a34e0
Livia Dantas <livia.ss.dantas@outlook.com>
2024-03-25 19:20:22 -0300

    Adding kafka consumer service

---
+++ PaymentService/Service/KafkaConsumerService.cs
@@ -0,0 +1,52 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+using static Confluent.Kafka.ConfigPropertyNames;
+
+namespace pay_admin.Service
+{
+    public abstract class KafkaConsumerService<T> : BackgroundService
+    {
+        private readonly IConsumer<string, string> _consumer;
+        public KafkaConsumerService(string bootstrapServers, string groupId, string topic)
+        {
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = bootstrapServers,
+                GroupId = groupId,
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+            _consumer = new ConsumerBuilder<string, string>(config).Build();
+            _consumer.Subscribe(topic); 
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while(!stoppingToken.IsCancellationRequested) 
+            {
+                try
+                {
+                    var result = _consumer.Consume(stoppingToken);
+                    if(result != null && !result.IsPartitionEOF)
+                    {
+                        var value = JsonConvert.DeserializeObject<T>(result.Value);
+                        await ConsumeAsync(value, stoppingToken);
+                    }
+                }catch (OperationCanceledException)
+                {
+                    throw;
+                }catch (Exception ex)
+                {
+                    throw new Exception(ex.ToString(), ex);
+                }
+            }
+            _consumer.Close();
+        }
+        public override void Dispose()
+        {
+            _consumer.Dispose();
+            base.Dispose();
+        }
+        protected abstract Task ConsumeAsync(T value, CancellationToken stoppingToken);
+
+
+    }
+}
