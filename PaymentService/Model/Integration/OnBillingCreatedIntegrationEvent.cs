fc39c58076c95a796296c80d9179614aec842438
Livia Dantas <livia.ss.dantas@outlook.com>
2024-03-29 20:54:11 -0300

    Creating a integration event class on billing has been created

--- PaymentService/Model/Integration/OnBillingCreatedIntegrationEvent.cs
+++ PaymentService/Model/Integration/OnBillingCreatedIntegrationEvent.cs
@@ -1,6 +1,14 @@
-﻿namespace pay_admin.Model.Integration
+﻿using Microsoft.AspNetCore.Identity;
+using System.Runtime.CompilerServices;
+
+namespace pay_admin.Model.Integration
 {
-    public class OnBillingCreatedIntegrationEvent
+    public class OnBillingCreatedIntegrationEventArgs : EventArgs
     {
+        public string IdMessage { get; set; }
+        public string IdUser { get; set; }
+        public string MessageContent { get; set; }
+
     }
+    public delegate void OnBillingCreatedIntegrationEventHandler(object sender, OnBillingCreatedIntegrationEventArgs eventArgs);
 }
